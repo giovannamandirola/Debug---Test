d06d8979a54cb14053978eebd4e942c208fa38f4
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-09-12 19:45:52 -0300

    Terminado

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,27 +6,68 @@ namespace Tests
 {
     public class Tests
     {
+        private Person person;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
-            this.person = new Person()
-            Person sofia = new Person("Test", "8.765.432-1");
+            person = new Person("Test", "1476358-9");
         }
 
         [Test]
-        public void NombreNoVacio() // Cambiá el nombre para indicar qué estás probando
+        public void NombreInvalido()
         {
-            // Insertá tu código  de pruebaaquí
-            sofia.nombre("");
-            Assert.AreEqual(sofia.nombre;"");
+            const string expected = "Test";
+            person.Name= "";
+            Assert.AreEqual(expected, person.Name);
         }
+
         [Test]
-        public void IdVálido() // Cambiá el nombre para indicar qué estás probando
+        public void NombreNull()
         {
-            // Insertá tu código  de pruebaaquí
-            sofia.id("8123452-4");
-            Assert.AreEqual(sofia.id; "");
+            const string expected = "Test";
+            person.Name= null;
+            Assert.AreEqual(expected, person.Name);
+        }
+        
+        [Test]
+        public void NombreValido()
+        {
+            const string expected = "Hola";
+            person.Name = expected;
+            Assert.AreEqual(expected, person.Name);
+        }
+
+        [Test]
+        public void IdInvalido()
+        {
+            const string expected = "1476358-9";
+            person.ID = "9999999-2";
+            Assert.AreEqual(expected, person.ID);
+        }
+
+        [Test]
+        public void IdInvalidoLetras()
+        {
+            const string expected = "1476358-9";
+            person.ID = "Hola";
+            Assert.AreEqual(expected, person.ID);
+        }
+
+        [Test]
+        public void IdVacio()
+        {
+            const string expected = "1476358-9";
+            person.ID = "";
+            Assert.AreEqual(expected, person.ID);
+        }
+
+        [Test]
+        public void IdValido()
+        {
+            const string expected = "6020931-5";
+            person.ID = expected;
+            Assert.AreEqual(expected, person.ID);
         }
     }
 }
